90c91b4daa217673ba043f0f3e4a64aba68cf50d
Filipe Alves Braga Rosa <54287231+FilipeAlvesPRG@users.noreply.github.com>
2022-07-04 16:23:59 -0300

    add dice images

--- app/src/main/java/com/example/rolling_dice/MainActivity.kt
+++ app/src/main/java/com/example/rolling_dice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rolling_dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -19,8 +20,15 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
