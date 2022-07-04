4222a666a72332cb53839777471c225c02c8cc25
Filipe Alves Braga Rosa <54287231+FilipeAlvesPRG@users.noreply.github.com>
2022-07-04 20:46:17 -0300

    Update a simple test

--- app/src/test/java/com/example/rolling_dice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rolling_dice/ExampleUnitTest.kt
@@ -9,9 +9,11 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class FirstUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
